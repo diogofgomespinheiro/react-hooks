6ce953933586d79c709f11b7bd4c6d38c98efed4
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-01-16 19:03:43 +0000

    finish ex06.extra-1

---
+++ src/exercise/06.extra-1.js
@@ -0,0 +1,62 @@
+// useEffect: HTTP requests
+// http://localhost:3000/isolated/exercise/06.js
+
+import * as React from 'react'
+import {fetchPokemon, PokemonInfoFallback, PokemonDataView} from '../pokemon'
+// 🐨 you'll want the following additional things from '../pokemon':
+// fetchPokemon: the function we call to get the pokemon info
+// PokemonInfoFallback: the thing we show while we're loading the pokemon info
+// PokemonDataView: the stuff we use to display the pokemon info
+import {PokemonForm} from '../pokemon'
+
+function PokemonInfo({pokemonName}) {
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    if (!pokemonName) return
+
+    setPokemon(null)
+    fetchPokemon(pokemonName)
+      .then(pokemonData => {
+        setPokemon(pokemonData)
+      })
+      .catch(error => setError(error))
+  }, [pokemonName])
+
+  if (Boolean(error))
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+
+  if (!pokemonName) return 'Submit a pokemon'
+
+  return Boolean(pokemon) ? (
+    <PokemonDataView pokemon={pokemon} />
+  ) : (
+    <PokemonInfoFallback name={pokemonName} />
+  )
+}
+
+function App() {
+  const [pokemonName, setPokemonName] = React.useState('')
+
+  function handleSubmit(newPokemonName) {
+    setPokemonName(newPokemonName)
+  }
+
+  return (
+    <div className="pokemon-info-app">
+      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
+      <hr />
+      <div className="pokemon-info">
+        <PokemonInfo pokemonName={pokemonName} />
+      </div>
+    </div>
+  )
+}
+
+export default App
